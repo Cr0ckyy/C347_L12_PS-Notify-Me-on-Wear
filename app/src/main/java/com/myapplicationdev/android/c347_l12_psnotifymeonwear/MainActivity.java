ed27d81d81ff18bd91ee39b96de723ef99e4b0a0
LI SHUFANG <lishufang1121@gmail.com>
2021-08-05 18:28:42 +0800

    test

---
+++ app/src/main/java/com/myapplicationdev/android/c347_l12_psnotifymeonwear/MainActivity.java
@@ -0,0 +1,64 @@
+package com.myapplicationdev.android.c347_l12_psnotifymeonwear;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnAdd;
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<Task> al;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        lv = findViewById(R.id.lv);
+
+        DBHelper dbh = new DBHelper(MainActivity.this);
+        al = dbh.getAllTask();
+        dbh.close();
+
+        aa = new ListAdapter(this, R.layout.row, al);
+        lv.setAdapter(aa);
+
+        btnAdd.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, AddActivity.class);
+            startActivity(i);
+        });
+
+        Intent intentreply = new Intent(MainActivity.this,
+                ReplyActivity.class);
+        PendingIntent pendingIntentReply = PendingIntent.getActivity
+                (MainActivity.this, 0, intentreply,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        RemoteInput ri = new RemoteInput.Builder("status")
+                .setLabel("Status report")
+                .setChoices(new String [] {"Done", "Not yet"})
+                .build();
+
+        NotificationCompat.Action action2 = new
+                NotificationCompat.Action.Builder(
+                R.mipmap.ic_launcher,
+                "Reply",
+                pendingIntentReply)
+                .addRemoteInput(ri)
+                .build();
+
+        NotificationCompat.WearableExtender extender = new
+                NotificationCompat.WearableExtender();
+        extender.addAction(action2);
+    }
+}
